f598e33962ad542d14ab243bd97c0245adcff2fa
polycirrus <polycirrus@gmail.com>
2015-04-17 08:51:32 +0300

    commit to github

---
+++ coursework_temp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coursework_temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ coursework_temp/SemanticAnalyzer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace coursework_temp
+{
+    class SemanticAnalyzer
+    {
+        String text;
+
+        public int CharacterCount(bool noWhitespaces)
+        {
+            if (noWhitespaces)
+                return Regex.Matches(text, "\\S").Count;
+            else
+                return Regex.Matches(text, ".").Count;
+        }
+
+        public String[] Words()
+        {
+            return Regex.Matches(text, "[A-Za-z]*(-[a-zA-Z]+)*").Cast<Match>().Select(match => match.Value).ToArray();
+        }
+
+        public String[] UniqueWords()
+        {
+            return Words().Distinct().ToArray();
+        }
+
+        public int WordCount()
+        {
+            return Words().Count();
+        }
+
+        public int UniqueWordCount()
+        {
+            return UniqueWords().Count();
+        }
+    }
+}
